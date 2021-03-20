9ec4b637a76e5a76262d8d086a61651e7a2e314c
azusaw <azusa0304@gmail.com>
2021-03-21 00:23:58 +0900

    create contact form

---
+++ components/logo/PaperPlane.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import Link from "next/link"
+import style from "./azsaw.module.css"
+
+const PaperPlane: React.FC = () => {
+  return (
+    <svg>
+      <defs id="defs2" />
+      <g id="layer1" transform="translate(30.485397,-190.81421)">
+        <path
+          id="rect847-6"
+          style={{
+            fill: "#f2f2f2",
+            fillOpacity: 1,
+            stroke: "#f2f2f2",
+            strokeWidth: "0.9",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4,
+            strokeDasharray: "none",
+            strokeOpacity: 1,
+          }}
+          d="M 7.0225455,245.89878 52.627664,196.2641 37.839345,260.52006 Z"
+        />
+        <path
+          id="rect847-4-3"
+          style={{
+            fill: "#999999",
+            fillOpacity: 1,
+            stroke: "#999999",
+            strokeWidth: "0.4",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4,
+            strokeDasharray: "none",
+            strokeOpacity: 1,
+          }}
+          d="M 1.0590966,256.90395 7.4630539,246.39504 -3.265684,240.86598 c 0.8964585,-0.54988 4.3247806,16.03797 4.3247806,16.03797 z"
+        />
+        <path
+          id="rect871-4"
+          style={{
+            fill: "#cccccc",
+            fillOpacity: 1,
+            stroke: "#cccccc",
+            strokeWidth: "0.5",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4,
+            strokeDasharray: "none",
+            strokeOpacity: 1,
+          }}
+          d="M 52.627664,196.2641 6.124852,245.87515 0.66965632,256.68729 -3.147447,242.25908 Z"
+        />
+        <path
+          id="rect847-7-5"
+          style={{
+            fill: "#ffffff",
+            fillOpacity: 1,
+            stroke: "#ffffff",
+            strokeWidth: "0.5",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4,
+            strokeDasharray: "none",
+            strokeOpacity: 1,
+          }}
+          d="M -25.235428,228.77005 52.627664,196.2641 c -17.386905,17.32391 -55.8265306,45.47051 -55.8265306,45.47051 z"
+        />
+      </g>
+    </svg>
+  )
+}
+export default PaperPlane
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,18 @@ import PageLayout from "../layouts/PageLayout"
 import colorStyle from "../styles/color.module.css"
 import commonStyle from "../styles/common.module.css"
 import sizeStyle from "../styles/size.module.css"
+import spaceStyle from "../styles/space.module.css"
+import { Button, Form, Input } from "antd"
+import { SendOutlined } from "@ant-design/icons"
+import PaperPlane from "../components/logo/PaperPlane"
+
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { offset: 2, span: 16 },
+}
+const tailLayout = {
+  wrapperCol: { offset: 7, span: 16 },
+}
 
 export default function Home() {
   return (
@@ -19,7 +31,43 @@ export default function Home() {
         {"Want to work together?  -> "}
         <span className={commonStyle.bold}> {"Send me a message."}</span>
       </div>
-      <div style={{ height: "100vh" }}>dummy</div>
+      <div className={commonStyle.box}>
+        <div className={colorStyle.bold}>
+          <SendOutlined className={sizeStyle.fontXLarge} />
+          <PaperPlane />
+        </div>
+        <Form {...layout} colon={false}>
+          <Form.Item
+            name="email"
+            label={<span className={colorStyle.white}>{"Email"}</span>}
+            rules={[
+              { required: true, message: "Please input your email address." },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="name"
+            label={<span className={colorStyle.white}>{"Name"}</span>}
+            rules={[{ required: true, message: "Please input your name." }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="messages"
+            label={<span className={colorStyle.white}>{"Messages"}</span>}
+            rules={[{ required: true, message: "Please input messages." }]}
+          >
+            <Input.TextArea rows={5} />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" block>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div style={{ height: "100vh", color: "#fff" }}>{"dummy"}</div>
     </PageLayout>
   )
 }
