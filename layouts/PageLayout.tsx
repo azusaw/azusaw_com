504ecfb2223c8cbd4d402e0327a8e57cb6fce1d3
azusaw <azusa0304@gmail.com>
2021-03-20 15:39:59 +0900

    fixed layout

--- layouts/PageLayout.tsx
+++ layouts/PageLayout.tsx
@@ -1,17 +1,33 @@
 import React from "react"
+import Link from "next/link"
 import Head from "next/head"
+import commonStyle from "../styles/common.module.css"
 import spaceStyle from "../styles/space.module.css"
+import Azusaw from "../components/logo/Azusaw"
 
 export default function PageLayout({ title, children }) {
-    return (
-      <div style={{backgroundColor: "#222228", opacity: 1, minHeight: "100vh"}}>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <div className={spaceStyle.mxAuto} style={{maxWidth: "60rem"}}>
-        {children}
+  return (
+    <div style={{ backgroundColor: "#222228", minHeight: "200vh" }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className={spaceStyle.mxAuto} style={{ maxWidth: "60rem" }}>
+        <div
+          className={spaceStyle.pa05}
+          style={{ position: "fixed", top: 0, height: "50px" }}
+        >
+          <Link href="/">
+            <div className={`${commonStyle.pointer} ${spaceStyle.ma10}`}>
+              <Azusaw />
+            </div>
+          </Link>
+        </div>
+        <div style={{ position: "relative", paddingTop: "60px" }}>
+          <div className={`${commonStyle.relative} ${spaceStyle.pa20}`}>
+            {children}
+          </div>
         </div>
       </div>
-    )
-
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import React from "react"
-import Head from "next/head"
-import Azusaw from "../components/logo/Azusaw"
 import PageLayout from "../layouts/PageLayout"
+import colorStyle from "../styles/color.module.css"
 import spaceStyle from "../styles/space.module.css"
 
 export default function Home() {
   return (
     <PageLayout title={"azusaw.com"}>
-      <div className={spaceStyle.pa10}>
-        <Azusaw />
+      <div className={colorStyle.white}>
+        {"I am a software engineer from Japan."}
+        <br />
+        {"I'm in the journey to improve my development skills and myself."}
       </div>
     </PageLayout>
   )
