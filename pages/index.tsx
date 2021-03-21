4d82ebd5b530a62fddbfe4a55cc3b9a47e651ce1
azusaw <azusa0304@gmail.com>
2021-03-21 22:57:12 +0900

    create new animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,41 @@
 import React from "react"
-import { motion } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
 import PageLayout from "../layouts/PageLayout"
 import colorStyle from "../styles/color.module.css"
 import commonStyle from "../styles/common.module.css"
 import sizeStyle from "../styles/size.module.css"
 import spaceStyle from "../styles/space.module.css"
-import { Button, Form, Input } from "antd"
-import PaperPlane from "../components/logo/PaperPlane"
+import { Button, Form, Input, Image } from "antd"
 import { LikeFilled, MailFilled } from "@ant-design/icons"
 
 const layout = {
   labelCol: { span: 5 },
-  wrapperCol: { offset: 2, span: 16 },
+  wrapperCol: { sm: { span: 24 }, md: { offset: 1, span: 17 } },
 }
 const tailLayout = {
-  wrapperCol: { offset: 7, span: 16 },
+  wrapperCol: { sm: { span: 24 }, md: { offset: 6, span: 17 } },
 }
 
-export default function Home() {
+const IndexPage: React.FC = () => {
+  const controls = useAnimation()
+  const mailSendAnimationStart = () => {
+    controls.start({
+      x: 250,
+      y: -200,
+      transformOrigin: "center bottom",
+      rotate: 180,
+      opacity: [1, 1, 0],
+      transition: {
+        ease: "linear",
+        duration: 1.8,
+        repeat: 1,
+        repeatType: "reverse",
+        repeatDelay: 5,
+      },
+    })
+  }
   return (
-    <PageLayout title={"azusaw.com"}>
+    <PageLayout title={"azusaw.work"}>
       <div className={`${colorStyle.white} ${sizeStyle.lineLarge} `}>
         {"I'm a software engineer in Tokyo."}
         <br />
@@ -47,7 +63,21 @@ export default function Home() {
         </div>
       </div>
       <div className={commonStyle.box}>
-        <PaperPlane />
+        <h2
+          className={commonStyle.head}
+          style={{ display: "inline-block", position: "relative", top: 0 }}
+        >
+          {"CONTACT"}
+        </h2>
+        <motion.span animate={controls} style={{ position: "absolute" }}>
+          <Image
+            preview={false}
+            width={45}
+            src="/images/paper-plane1.svg"
+            className={spaceStyle.ml10}
+            onClick={mailSendAnimationStart}
+          />
+        </motion.span>
         <Form {...layout} colon={false}>
           <Form.Item
             name="email"
@@ -88,3 +118,4 @@ export default function Home() {
     </PageLayout>
   )
 }
+export default IndexPage
