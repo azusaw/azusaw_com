55d35a51d3a0a891458baca6753a6530d866e946
azusaw <azusa0304@gmail.com>
2021-07-18 16:14:23 +0900

    add skill icons

--- components/logo/WhatICanDo.tsx
+++ components/logo/WhatICanDo.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { motion, useAnimation } from "framer-motion"
-import { Button, Form, Image, Input, message } from "antd"
+import { Avatar, Button, Col, Form, Image, Input, message } from "antd"
 import { HeartFilled } from "@ant-design/icons"
 import { useForm } from "antd/lib/form/Form"
 import colorStyle from "../../styles/color.module.css"
@@ -9,10 +9,34 @@ import sizeStyle from "../../styles/size.module.css"
 import spaceStyle from "../../styles/space.module.css"
 
 const WhatICanDo: React.FC = () => {
+  const items = [
+    { src: "/icons/react.svg", alt: "React.js" },
+    { src: "/icons/nextjs.svg", alt: "Next.js" },
+    { src: "/icons/gatsbyjs.svg", alt: "Gatsby.js" },
+    { src: "/icons/vue.svg", alt: "Vue.js" },
+    { src: "/icons/nuxtjs.svg", alt: "Nuxt.js" },
+    { src: "/icons/typescript.svg", alt: "typescript" },
+    { src: "/icons/html5_white.svg", alt: "html5" },
+    { src: "/icons/css3_white.svg", alt: "css3" },
+    { src: "/icons/aws_white.svg", alt: "AWS" },
+    { src: "/icons/python.svg", alt: "Python" },
+    { src: "/icons/flutter.svg", alt: "Flutter" },
+    { src: "/icons/java.svg", alt: "Java" },
+  ]
   return (
     <div className={commonStyle.container}>
       <h2 className={commonStyle.head}>{"WHAT I CAN DO"}</h2>
-      {"React.js, Next.js"}
+      <div className={commonStyle.logoBox}>
+        {items.map((item) => (
+          <Avatar
+            key={item.alt}
+            size={50}
+            src={item.src}
+            alt={item.alt}
+            className={`${spaceStyle.mx05} ${commonStyle.techLogo}`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
